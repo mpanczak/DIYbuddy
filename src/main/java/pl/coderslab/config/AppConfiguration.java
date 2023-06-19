f5db50262dcf4bdffaeb431a9ac0ebc671d87b01
mpanczak <panczakmateusz@gmail.com>
2023-06-19 22:35:51 +0200

    hibernate config

--- src/main/java/pl/coderslab/config/AppConfiguration.java
+++ src/main/java/pl/coderslab/config/AppConfiguration.java
@@ -3,9 +3,13 @@ package pl.coderslab.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableWebMvc
@@ -19,4 +23,19 @@ public class AppConfiguration implements WebMvcConfigurer {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean entityManagerFactoryBean
+                = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("diyBuddyPersistenceUnit");
+        return entityManagerFactoryBean;
+    }
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager jpaTransactionManager =
+                new JpaTransactionManager(entityManagerFactory);
+        return jpaTransactionManager;
+    }
+
 }
