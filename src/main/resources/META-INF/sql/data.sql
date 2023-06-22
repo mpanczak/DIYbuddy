376623f33b2828ed247509d2c235b5bf2fe85584
mpanczak <panczakmateusz@gmail.com>
2023-06-22 11:58:29 +0200

    load data from sql script

---
+++ src/main/resources/META-INF/sql/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (email, login, password) VALUES ('johndoe@gmail.com', 'johndoe', 'secret');
+INSERT INTO users (email, login, password) VALUES ('mpanczak@gmail.com', 'mpanczak', 'changeit');
